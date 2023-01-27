7b6060a06caee38c4558b947f3e6e42c4183f5d5
taeyuning <121299307+taeyun11@users.noreply.github.com>
2023-01-27 20:02:36 +0900

    logo

--- test_app/src/component/layouts/Header.jsx
+++ test_app/src/component/layouts/Header.jsx
@@ -3,24 +3,25 @@ import styles from '../layouts/Header.module.css';
 
 function Header() {
 
-return (  
-<header className={styles.header}>
- <div className={styles.contents}>
+    return (
+        <header className={styles.header}>
+        <div className={styles.contents}>
 
-<div>
-<h1>로고 자리</h1>
-</div>
+        {/* 로고 사이즈 조절하기ㅠㅠ */}
+       <h1 id="logo">
+            <a href="#"><img src="./images/logo192.png"/></a> 
+            </h1>
+                    <nav className={styles.navigation}>
+                        <ul>
+                            <li> Menu </li>
+                            <li> Cart </li>
+                            <li> Store</li>
+                        </ul>
+                    </nav>
 
-<nav className={styles.navigation}> 
-    <ul>
-        <li> Menu </li>
-        <li> Cart </li>
-        <li> Store</li>
-    </ul>
-</nav>
+            </div>
 
-</div>
-</header>
+        </header>
 
 
     );
