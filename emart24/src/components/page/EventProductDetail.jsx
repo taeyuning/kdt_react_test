0fe8b58993fccbb252176be9ab8f16f22f4b0a23
taeyuning <121299307+taeyun11@users.noreply.github.com>
2023-02-13 02:43:06 +0900

    매장, 상품상세

--- emart24/src/components/page/EventProductDetail.jsx
+++ emart24/src/components/page/EventProductDetail.jsx
@@ -5,12 +5,10 @@ import style from "./EventProductDetail.module.css";
 
 function EventProductDetail() {
   const { id } = useParams();
-  
-  console.log(id);
-
   const [productDetail, setProductDetail] = useState();
 
   useEffect(() => {
+    console.log(id);
     fetch(`http://localhost:3001/products/${id}`)
       .then((res) => res.json())
       .then((data) => {
@@ -19,17 +17,32 @@ function EventProductDetail() {
       });
   }, []);
 
-
+//물어보기//
   return (
-<section>
- {productDetail && productDetail.map((prod) =>(
-<></>
+        <div>
 
- ))}
-</section>
-  );
+           {productDetail && productDetail.map((prod) => (
+           <div className={style.productWrap}>
+           <div className={style.thumbnail}>
+            <img src={prod.thumbnail} alt="product 01" />
+            </div>
+            <div className={style.info}>
+            <h2>{prod.name}</h2>
+            <p>{prod.price}원</p>
+            <p>{prod.category}</p>
+            <p>{prod.brand}</p>
+            <p>{prod.description}</p>
+            </div>
+            </div>
+            ))}
+            
+               </div>
+        
+        );
+        }
+      
+   
 
 
-  }
 
 export default EventProductDetail;
--- emart24/src/components/ui/main/MainBanner.jsx
+++ emart24/src/components/ui/main/MainBanner.jsx
@@ -10,10 +10,11 @@ function MainBanner() {
     <input type="radio" name="slide" id="slide1" checked/>
     <input type="radio" name="slide" id="slide2"/>
     <input type="radio" name="slide" id="slide3"/>
+
     <ul id="imgholder" className={style.img}>
-        <li><img src="https://emart24.co.kr/image/NTAwOA=="/></li>
-        <li><img src="https://emart24.co.kr/image/NDk3Mg=="/></li>
-        <li><img src="https://emart24.co.kr/image/NDg1OA=="/></li> 
+        <li><img src="https://emart24.co.kr/image/NTAwOA==" alt=''/></li>
+        <li><img src="https://emart24.co.kr/image/NDk3Mg==" alt=''/></li>
+        <li><img src="https://emart24.co.kr/image/NDg1OA==" alt=''/></li> 
     </ul>
     <div className={style.bullet}>
         <label for="slide1">&nbsp;</label>
@@ -28,3 +29,4 @@ function MainBanner() {
 }
 
 export default MainBanner;
+//수정하기
